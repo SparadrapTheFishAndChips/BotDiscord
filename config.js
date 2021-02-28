fbb63809836a818c7080e375c86bdd4f4a26225a
FishAndChip's <61520769+SparadrapTheFishAndChips@users.noreply.github.com>
2021-02-28 19:01:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-exports.TOKEN = "NjgyMjk0MzY5MTMwMjUwMzAy.XtLLBg.uh8h4y7PDrkniIvt5O6iFPJ9bJw";
-exports.PREFIX = "!wp";
-exports.AUTHORURL = "http://sparadrap.atspace.eu/";
-exports.WELCOMECHAN = "welcome";
-exports.NEWROLE = "New";
-exports.PERMLINK = "https://discord.gg/7smvTnV";
+
