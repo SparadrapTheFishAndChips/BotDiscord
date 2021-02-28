dafa8b2555c001bb7090a795b13f417e03ad10f4
FishAndChip's <61520769+SparadrapTheFishAndChips@users.noreply.github.com>
2021-02-28 19:36:06 +0100

    Update and rename config.js to Sample-config.js

---
+++ Sample-config.js
@@ -0,0 +1,10 @@
+//Token du bot
+exports.TOKEN = "TOKEN";
+//Prefix pour les fonction du bot
+exports.PREFIX = "!bot";
+//Message de bienvenue sur le chan. laisser vide si non utilisé :
+exports.WELCOMECHAN = "[Nom du Chan. de bienvenue]";
+//Nom du role (string) pour l'attribution
+exports.NEWROLE = "New";
+//Url permanent du lien (laisser vide si non utilisé)
+exports.PERMLINK = "https://discord.gg/7smvTnV";
